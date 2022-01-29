5623cbfb11eb1a971ed9072728e2e83a468e6823
mak-leibniz <makmiller@gmail.com>
2022-01-29 11:13:43 -0500

    Write function to extract time from trackpoints

---
+++ get_hr_from_tcx.py
@@ -0,0 +1,28 @@
+import xml.etree.ElementTree as ET
+# import pandas as pd
+
+with open("teste.tcx") as f:
+    tcx = ET.parse(f)
+
+# TCX file hieararchy:
+# Root -> Activities -> Activity -> Lap -> Track -> Trackpoint
+
+
+root = tcx.getroot()
+
+nms_base = '{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}'
+
+datapoints = list(root.iter(nms_base + 'Trackpoint'))
+
+
+def get_time_from_trackpoint(p):
+    elem = p.find(nms_base + 'Time')
+    return elem.text
+
+
+def get_time_from_datapoints(d):
+    lst = [0]
+    for i in range(1, len(d)):
+        t = get_time_from_trackpoint(d[i])
+        lst.append(t)
+    return lst
