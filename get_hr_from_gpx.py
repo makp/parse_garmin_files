2a133b57b23cf15ceebf841e3ac0de3b9acb35b9
mak-leibniz <makmiller@gmail.com>
2022-01-29 23:26:48 -0500

    Comment code

--- get_hr_from_gpx.py
+++ get_hr_from_gpx.py
@@ -1,5 +1,5 @@
 import gpxpy
-import pandas as pd
+# import pandas as pd
 
 with open("teste.gpx") as f:
     gpx = gpxpy.parse(f)
@@ -21,36 +21,36 @@ activity_type = activity.type
 datapoints = gpx.get_points_data()
 
 
-def get_time_datapoint(p):
-    return p.point.time
+# def get_time_datapoint(p):
+#     return p.point.time
 
 
-def get_time_increments(lst):
-    """ Calculates the time interval between HR readings.
-FIXME: The drawkback of using time increments calculated as above is
-that it doesn't take into account when you pause the activity."""
-    new_lst = [0]
-    for i in range(1, len(lst)):
-        diff = lst[i] - lst[i-1]
-        new_lst.append(diff.total_seconds())
-    return new_lst
+# def get_time_increments(lst):
+#     """ Calculates the time interval between HR readings.
+# FIXME: The drawkback of using time increments calculated as above is
+# that it doesn't take into account when you pause the activity."""
+#     new_lst = [0]
+#     for i in range(1, len(lst)):
+#         diff = lst[i] - lst[i-1]
+#         new_lst.append(diff.total_seconds())
+#     return new_lst
 
 
-def get_hr_datapoint(p):
-    assert len(p.point.extensions) == 1, "Datapoint contains 1+ extensions!!"
-    elem = p.point.extensions[0]
-    nsp_base = "{" + list(elem.nsmap.values())[0] + "}"
-    nsp_hr = nsp_base + 'hr'
-    ext_hr = elem.find(nsp_hr)
-    try:
-        hr = int(ext_hr.text)
-    except AttributeError:
-        hr = None
-    return hr
+# def get_hr_datapoint(p):
+#     assert len(p.point.extensions) == 1, "Datapoint contains 1+ extensions!!"
+#     elem = p.point.extensions[0]
+#     nsp_base = "{" + list(elem.nsmap.values())[0] + "}"
+#     nsp_hr = nsp_base + 'hr'
+#     ext_hr = elem.find(nsp_hr)
+#     try:
+#         hr = int(ext_hr.text)
+#     except AttributeError:
+#         hr = None
+#     return hr
 
 
-ts = list(map(get_time_datapoint, datapoints))
-delta = get_time_increments(ts)
-hrs = list(map(get_hr_datapoint, datapoints))
+# ts = list(map(get_time_datapoint, datapoints))
+# delta = get_time_increments(ts)
+# hrs = list(map(get_hr_datapoint, datapoints))
 
-df = pd.DataFrame(zip(delta, hrs), columns=['Secs', 'HRs'])
+# df = pd.DataFrame(zip(delta, hrs), columns=['Secs', 'HRs'])
