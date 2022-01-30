066058d336b7e87a93e7289397cdabf8d1b8081f
mak-leibniz <makmiller@gmail.com>
2022-01-29 21:00:07 -0500

    Loop over tracks to calculate time increments
    Otherwise the calculated time increments will include the periods an
    activity was paused.

--- get_hr_from_tcx.py
+++ get_hr_from_tcx.py
@@ -13,7 +13,7 @@ root = tcx.getroot()
 
 nms_base = '{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}'
 
-datapoints = list(root.iter(nms_base + 'Trackpoint'))
+tracks = list(root.iter(nms_base + 'Track'))
 
 
 def get_time_from_trackpoint(p):
@@ -21,20 +21,17 @@ def get_time_from_trackpoint(p):
     return elem.text
 
 
-def get_time_from_datapoints(d):
+def get_track_timestamps(track):
     lst = []
-    for i in range(0, len(d)):
-        s = get_time_from_trackpoint(d[i])
-        s2 = s.split('T')[-1].split('.')[0]
+    for p in track:
+        s1 = get_time_from_trackpoint(p)
+        s2 = s1.split('T')[-1].split('.')[0]
         t = dt.datetime.strptime(s2, '%H:%M:%S')
         lst.append(t)
     return lst
 
 
-def calculate_time_increments(lst):
-    """ Calculates the time interval between HR readings.
-FIXME: The drawkback of using time increments calculated as above is
-that it doesn't take into account when you pause the activity."""
+def calc_increments(lst):
     new_lst = [0]
     for i in range(1, len(lst)):
         diff = lst[i] - lst[i-1]
@@ -42,14 +39,49 @@ that it doesn't take into account when you pause the activity."""
     return new_lst
 
 
+def calc_time_increments(tracks):
+    out = []
+    for track in tracks:
+        lst = get_track_timestamps(track)
+        lst_inc = calc_increments(lst)
+        out.extend(lst_inc)
+    return out
+
+
+
+# def get_time_from_datapoints(d):
+#     lst = []
+#     for i in range(0, len(d)):
+#         s = get_time_from_trackpoint(d[i])
+#         s2 = s.split('T')[-1].split('.')[0]
+#         t = dt.datetime.strptime(s2, '%H:%M:%S')
+#         lst.append(t)
+#     return lst
+
+
+# def calculate_time_increments(lst):
+#     """ Calculates the time interval between HR readings.
+# FIXME: The drawkback of using time increments calculated as above is
+# that it doesn't take into account when you pause the activity. Maybe Garmin
+# creates a new Track every time the activity is paused."""
+#     new_lst = [0]
+#     for i in range(1, len(lst)):
+#         diff = lst[i] - lst[i-1]
+#         new_lst.append(diff.total_seconds())
+#     return new_lst
+
+datapoints = list(root.iter(nms_base + 'Trackpoint'))
+
+
 def get_hr_from_trackpoint(p):
+    """Get HR from Trackpoint.
+FIXME: Deal with cases when HR not present."""
     elem = p.find(nms_base + 'HeartRateBpm')
     s = elem.find(nms_base + 'Value').text
     return int(s)
 
 
 HRs = list(map(get_hr_from_trackpoint, datapoints))
-ts = get_time_from_datapoints(datapoints)
-deltas = calculate_time_increments(ts)
+deltas = calc_time_increments(tracks)
 
 df = pd.DataFrame(zip(deltas, HRs), columns=['Secs', 'HRs'])
