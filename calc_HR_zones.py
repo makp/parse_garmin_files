b6470434ae84e2e0f4b3f050331fb6216e83d024
mak-leibniz <makmiller@gmail.com>
2022-01-30 23:38:57 -0500

    Create a single pandas df for all the files

--- calc_HR_zones.py
+++ calc_HR_zones.py
@@ -1,4 +1,5 @@
 from get_hr_from_tcx import create_pandas_with_hrs
+import pandas as pd
 
 
 filenames = ['teste']
@@ -6,7 +7,13 @@ filenames = ['teste']
 zone_ranges = {'z1/z2': (113, 154), 'z3': (155, 164),
                'z4': (165, 173), 'z5': (174, 200)}
 
-df_hrs = create_pandas_with_hrs(filenames[0])
+
+def create_df_with_hrs(lst):
+    out = pd.DataFrame([])
+    for f in lst:
+        df = create_pandas_with_hrs(f)
+        out = out.append(df)
+    return out
 
 
 def calc_time_each_zone(df):
