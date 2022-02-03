d6b504ea997b657e547b6430ce28cfc51a6d6935
mak-leibniz <makmiller@gmail.com>
2022-02-03 00:23:27 -0500

    Mv vars to the end of the file

--- select_workouts.py
+++ select_workouts.py
@@ -18,11 +18,6 @@ def get_date_from_filename(s):
     return utc.astimezone(local_tz).date()
 
 
-workout_dates = [get_date_from_filename(s) for s in lst_files]
-df_workouts = pd.DataFrame(zip(workout_dates, lst_files),
-                           columns=['date', 'file'])
-
-
 def get_date_last_monday():
     """Return date object corresponding to last Monday."""
     today = dt.datetime.now()
@@ -38,7 +33,10 @@ def get_date_n_days_ago(n):
     return out.date()
 
 
+workout_dates = [get_date_from_filename(s) for s in lst_files]
+df_workouts = pd.DataFrame(zip(workout_dates, lst_files),
+                           columns=['date', 'file'])
+
 tstart = get_date_last_monday()
 tend = get_date_n_days_ago(0)
-
 df_selection = df_workouts.query('date >= @tstart and date <= @tend')
