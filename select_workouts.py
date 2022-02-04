941de7daa8c30c7663f484a420ce80adbe7de2c6
mak-leibniz <makmiller@gmail.com>
2022-02-03 20:34:49 -0500

    Bundle statements into a function

--- select_workouts.py
+++ select_workouts.py
@@ -22,24 +22,30 @@ def get_date_from_filename(s):
 
 
 def get_date_last_monday():
-    """Return date object corresponding to last Monday."""
+    """Return date object corresponding to last Monday.
+    TODO: Get date from a particular weekday (not just Monday)."""
     today = dt.datetime.now()
     ind = today.weekday()
     t = today - dt.timedelta(days=ind)
     return t.date()
 
 
-def get_date_n_days_ago(n):
-    """Return date object for n days ago."""
-    today = dt.datetime.now()
-    out = today - dt.timedelta(days=n)
-    return out.date()
+def get_date_n_days_ago(val):
+    """Return date object for n days ago or last Monday."""
+    if val == "Mon":
+        out = get_date_last_monday()
+    else:
+        today = dt.datetime.now()
+        diff = today - dt.timedelta(days=val)
+        out = diff.date()
+    return out
 
 
-workout_dates = [get_date_from_filename(s) for s in lst_files]
-df_workouts = pd.DataFrame(zip(workout_dates, lst_files),
-                           columns=['date', 'file'])
-
-tstart = get_date_last_monday()
-tend = get_date_n_days_ago(0)
-df_selection = df_workouts.query('date >= @tstart and date <= @tend')
+def select_workouts_between_dates(t0="Mon", t1=0):
+    workout_dates = [get_date_from_filename(s) for s in lst_files]
+    df_workouts = pd.DataFrame(zip(workout_dates, lst_files),
+                               columns=['date', 'file'])
+    tstart = get_date_last_monday()
+    tend = get_date_n_days_ago(t1)
+    df_selection = df_workouts.query('date >= @tstart and date <= @tend')
+    return df_selection.sort_values(by=['date'])
