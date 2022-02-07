35aee19118fb00e1d5f0c14473f12c3792376fed
mak-leibniz <makmiller@gmail.com>
2022-02-07 00:44:57 -0500

    Bundle statements into different funcs

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ from select_workouts_between_dates import select_workouts_between_dates
 from get_info_about_workout import get_info_from_files
 from calc_HR_zones import create_pandas_with_zones
 
-#  TODO: Streamline code.
 
-df1 = select_workouts_between_dates()
-df2 = get_info_from_files(df1.loc[:, 'file'])
-df3 = pd.concat([df1, df2], axis=1)
-df3.query("type == 'treadmill_running' or type == 'running'", inplace=True)
-df3.reset_index(drop=True, inplace=True)
-df4 = create_pandas_with_zones(df3['file'])
-df5 = pd.concat([df3, df4], axis=1)
-df5.loc[:, 'z1/z2':'z5'] = round(df5.loc[:, 'z1/z2':'z5']/60, 1)
-df5.drop('file', axis=1, inplace=True)
+def list_workouts(t0="Mon", t1=0):
+    """List workouts and their info within a time range."""
+    df1 = select_workouts_between_dates(t0, t1)
+    df2 = get_info_from_files(df1.loc[:, 'file'])
+    return pd.concat([df1, df2], axis=1)
+
+
+def filter_workouts(df, key='running'):
+    if key == 'running':
+        out = df.query("type == 'treadmill_running' or type == 'running'")
+    out.reset_index(drop=True, inplace=True)
+    return out
+
+
+def add_time_zones(df):
+    df_zones = create_pandas_with_zones(df['file'])
+    out = pd.concat([df, df_zones], axis=1)
+    out.rename(columns={'Secs': 'Mins'}, inplace=True)
+    return round(out.loc[:, 'z1/z2':'z5']/60, 1)
+
+
+
+# df5.drop('file', axis=1, inplace=True)
