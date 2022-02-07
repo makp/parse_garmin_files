18a2ac0d943f2955f6d26c7acf7e8ae6279a0f52
mak-leibniz <makmiller@gmail.com>
2022-02-06 23:24:40 -0500

    List HR zone times for each run

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pandas as pd
 from select_workouts_between_dates import select_workouts_between_dates
 from get_info_about_workout import get_info_from_files
+from calc_HR_zones import create_pandas_with_zones
 
+#  TODO: Streamline code.
 
 df1 = select_workouts_between_dates()
-df1.reset_index(drop=True, inplace=True)
 df2 = get_info_from_files(df1.loc[:, 'file'])
 df3 = pd.concat([df1, df2], axis=1)
-df3.drop('file', axis=1, inplace=True)
-df3.query("type == 'treadmill_running' or type == 'running'")
+df3.query("type == 'treadmill_running' or type == 'running'", inplace=True)
+df3.reset_index(drop=True, inplace=True)
+df4 = create_pandas_with_zones(df3['file'])
+df5 = pd.concat([df3, df4], axis=1)
+df5.loc[:, 'z1/z2':'z5'] = round(df5.loc[:, 'z1/z2':'z5']/60, 1)
+df5.drop('file', axis=1, inplace=True)
