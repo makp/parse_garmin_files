813d2a09f9421c7cc2178369efe76b0df5a702e4
mak-leibniz <makmiller@gmail.com>
2022-02-07 23:03:55 -0500

    Add docstring; Change func name

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from get_info_about_workout import get_info_from_files
 from calc_HR_zones import create_pandas_with_zones
 
 
-def list_workouts(t0="Mon", t1=0):
+def lst_workouts(t0="Mon", t1=0):
     """List workouts and their info within a time range."""
     df1 = select_workouts_between_dates(t0, t1)
     df2 = get_info_from_files(df1.loc[:, 'file'])
@@ -12,6 +12,7 @@ def list_workouts(t0="Mon", t1=0):
 
 
 def filter_workouts(df, key='running'):
+    """Filter workouts based on the type (e.g., running)."""
     if key == 'running':
         out = df.query("type == 'treadmill_running' or type == 'running'")
     out.reset_index(drop=True, inplace=True)
