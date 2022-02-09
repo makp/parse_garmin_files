290d329e2b6f3fba98e4a66e081ba7b49a797fb8
mak-leibniz <makmiller@gmail.com>
2022-02-09 00:09:27 -0500

    Clean file

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pandas as pd
-from select_workouts_between_dates import select_workouts_between_dates
-from get_info_about_workout import get_info_from_files
-from calc_HR_zones import create_df_with_zones
-
-
-def filter_workouts_by_date(df, t0="Mon", t1=0):
-    """List workouts and their info within a time range."""
-    df1 = select_workouts_between_dates(t0, t1)
-    df2 = get_info_from_files(df1.loc[:, 'file'])
-    return pd.concat([df1, df2], axis=1)
+from lst_workout_between_dates import select_workouts_between_dates
 
 
 def filter_workouts_by_type(df, key='running'):
@@ -25,3 +15,6 @@ def filter_workouts_by_type(df, key='running'):
         out = df.query(cond_aerobic)
     out.reset_index(drop=True, inplace=True)
     return out
+
+
+df = select_workouts_between_dates(14)
