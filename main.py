c03477c11ad3fca48e351516673ed436ccf9b8a2
mak-leibniz <makmiller@gmail.com>
2022-02-08 21:21:33 -0500

    Renames funcs

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from get_info_about_workout import get_info_from_files
 from calc_HR_zones import create_df_with_zones
 
 
-def lst_workouts(t0="Mon", t1=0):
+def filter_workouts_by_date(t0="Mon", t1=0):
     """List workouts and their info within a time range."""
     df1 = select_workouts_between_dates(t0, t1)
     df2 = get_info_from_files(df1.loc[:, 'file'])
     return pd.concat([df1, df2], axis=1)
 
 
-def filter_workouts(df, key='running'):
+def filter_workouts_by_type(df, key='running'):
     """Filter workouts based on the type (e.g., running)."""
     if key == 'running':
         out = df.query("type == 'treadmill_running' or type == 'running'")
