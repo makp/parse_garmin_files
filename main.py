4ddf3fd1504f421c1806e54712b672168852e2cd
mak-leibniz <makmiller@gmail.com>
2022-02-09 00:17:26 -0500

    Add zones

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from lst_workout_between_dates import select_workouts_between_dates
 from filter_workouts import filter_workouts_by_type
+from calc_HR_zones import add_time_zones
 
 
 df = select_workouts_between_dates(14)
-filter_workouts_by_type(df)
+df_running = add_time_zones(filter_workouts_by_type(df))
