4e5c84f25ec86ecce1f1343a9fcd1070a0ea3d5d
mak-leibniz <makmiller@gmail.com>
2022-02-09 00:09:06 -0500

    Add more options for filtering workouts

--- main.py
+++ main.py
@@ -12,8 +12,16 @@ def filter_workouts_by_date(df, t0="Mon", t1=0):
 
 
 def filter_workouts_by_type(df, key='running'):
-    """Filter workouts based on the type (e.g., running)."""
+    """Filter workouts based on whether they are cycling, running, or
+    aerobic activities."""
+    cond_running = "type == 'treadmill_running' or type == 'running'"
+    cond_cycling = "type == 'indoor_cycling' or type == 'cyclying'"
+    cond_aerobic = cond_running + " or " + cond_cycling
     if key == 'running':
-        out = df.query("type == 'treadmill_running' or type == 'running'")
+        out = df.query(cond_running)
+    elif key == 'cycling':
+        out = df.query(cond_cycling)
+    elif key == 'aerobic':
+        out = df.query(cond_aerobic)
     out.reset_index(drop=True, inplace=True)
     return out
