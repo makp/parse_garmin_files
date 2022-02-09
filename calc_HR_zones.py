714b49e2a9bc7dac994e15517723d6a4ad3f86a4
mak-leibniz <makmiller@gmail.com>
2022-02-08 21:15:46 -0500

    Rewrite funcs

--- calc_HR_zones.py
+++ calc_HR_zones.py
@@ -6,24 +6,24 @@ zone_ranges = {'z1/z2': (113, 154), 'z3': (155, 164),
                'z4': (165, 173), 'z5': (174, 200)}
 
 
-def create_dic_with_hrs(filepath):
-    """Create a dictionary with time in seconds for each of the zones
-    as specified by 'zone-ranges.'"""
+def create_lst_with_zones(filepath):
+    """Return a list with the total number of seconds spent at each
+    zone---as specified by 'zone-ranges.'"""
     df = create_pandas_with_hrs(filepath)
-    dic = {}
-    for key in zone_ranges:
-        l, u = zone_ranges[key]
-        df_new = df.query('HRs >= @l and HRs <= @u')
-        dic[key] = df_new.loc[:, 'Secs'].sum()
-    return dic
+    lst = []
+    for val in zone_ranges.values():
+        l, u = val
+        t = df.query('HRs >= @l and HRs <= @u').loc[:, 'Secs'].sum()
+        lst.append(t)
+    return lst
 
 
-def create_pandas_with_zones(lst_filepath):
-    """Create pandas dataframe from a list of tcx files in which the
+def create_df_with_zones(lst_filepath):
+    """Return a pandas dataframe from a list of tcx files in which the
     columns list the amount of time spend at each HR zone---as
     specified by 'zone_ranges.'"""
-    df = pd.DataFrame()
+    df = pd.DataFrame([], columns=list(zone_ranges.keys()))
     for _ in lst_filepath:
-        dic = create_dic_with_hrs(_)
-        df = df.append(dic, ignore_index=True)
+        lst = create_lst_with_zones(_)
+        df.loc[df.shape[0]] = lst
     return df
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from select_workouts_between_dates import select_workouts_between_dates
 from get_info_about_workout import get_info_from_files
-from calc_HR_zones import create_pandas_with_zones
+from calc_HR_zones import create_df_with_zones
 
 
 def lst_workouts(t0="Mon", t1=0):
@@ -21,11 +21,7 @@ def filter_workouts(df, key='running'):
 
 def add_time_zones(df):
     """Add columns with the time spent at each zone."""
-    df_zones = create_pandas_with_zones(df['file'])
+    df_zones = create_df_with_zones(df['file'])
     out = pd.concat([df, df_zones], axis=1)
     out.loc[:, 'z1/z2':'z5'] = round(out.loc[:, 'z1/z2':'z5']/60, 1)
     return out
-
-
-
-# df5.drop('file', axis=1, inplace=True)
