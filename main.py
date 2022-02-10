157ad4ef013b07be7feffd7fd616e79e7618dfe0
mak-leibniz <makmiller@gmail.com>
2022-02-10 01:05:41 -0500

    Plot df using bars

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from lst_workout_between_dates import select_workouts_between_dates
 from filter_workouts import filter_workouts_by_type
 from calc_HR_zones import add_hr_zones
@@ -11,4 +13,12 @@ def calc_perc_per_zone(df):
 
 df = select_workouts_between_dates(31)
 
+
 df_running = add_hr_zones(filter_workouts_by_type(df))
+df_running.drop(['file', 'desc', 'type'], axis=1, inplace=True)
+
+df_aerobic = add_hr_zones(filter_workouts_by_type(df, 'aerobic'))
+
+
+df_running.plot(x='date', y=['z1/z2', 'z3', 'z4', 'z5'], width=0.9, kind='bar')
+plt.show()
