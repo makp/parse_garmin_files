b8a51c348b084d2c881065b574ebadd03b87c0b2
mak-leibniz <makmiller@gmail.com>
2022-02-10 01:03:12 -0500

    Add func to calc perc spent at each zone

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ from filter_workouts import filter_workouts_by_type
 from calc_HR_zones import add_hr_zones
 
 
-df = select_workouts_between_dates(14)
+def calc_perc_per_zone(df):
+    total_per_zone = df.loc[:, 'z1/z2':].sum()
+    total_time = total_per_zone.sum()
+    return total_per_zone/total_time
+
+
+df = select_workouts_between_dates(31)
+
 df_running = add_hr_zones(filter_workouts_by_type(df))
