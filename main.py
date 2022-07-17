8851ba09a87b939d892c33adbd6d5adbf302bad2
mak-leibniz <makmiller@gmail.com>
2022-07-16 20:34:54 -0400

    Update main file

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import matplotlib.pyplot as plt
 
-from lst_workout_between_dates import select_workouts_between_dates
-from filter_workouts import filter_workouts_by_type
+from filter_workouts import filter_workouts_by_date_and_type
 from calc_HR_zones import add_hr_zones
 
-
-def calc_perc_per_zone(df):
-    total_per_zone = df.loc[:, 'z1/z2':].sum()
-    total_time = total_per_zone.sum()
-    return total_per_zone/total_time
+df = filter_workouts_by_date_and_type(7)
 
 
-df = select_workouts_between_dates(31)
+if df.empty:
+    print("No workouts listed!!")
 
 
-df_running = add_hr_zones(filter_workouts_by_type(df))
-df_running.drop(['file', 'desc', 'type'], axis=1, inplace=True)
+df = add_hr_zones(df)
+df
 
-df_aerobic = add_hr_zones(filter_workouts_by_type(df, 'aerobic'))
+# def calc_perc_per_zone(df):
+#     total_per_zone = df.loc[:, 'z2':].sum()
+#     total_time = total_per_zone.sum()
+#     return total_per_zone/total_time
 
 
-df_running.plot(x='date', y=['z1/z2', 'z3', 'z4', 'z5'], width=0.9, kind='bar')
+df.plot(x='date', y=['z2', 'z3', 'z4', 'z5'], width=0.9, kind='bar',
+        color=['tab:blue', 'tab:green', 'gold', 'tab:red'])
+plt.ion()
 plt.show()
